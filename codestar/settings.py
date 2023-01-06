aa5cebe7e721348cbb52a23789628b68694f7edc
Damien <db.webapp@gmail.com>
2023-01-06 22:10:40 +0000

    editing the like feature for a post

--- codestar/settings.py
+++ codestar/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'django_summernote',
     'blog',
 ]
@@ -56,6 +57,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATES_PACK = 'bootstrap4'
 
 
 MIDDLEWARE = [
